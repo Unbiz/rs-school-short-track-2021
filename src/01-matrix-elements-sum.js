76c174544c69b6638b93b436fed38cd2d246681d
Vladimir Bessonov <bessmy@gmail.com>
2021-04-08 11:49:31 +0300

    Feat: done 01 and 02 tasks

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,17 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  const result = matrix.reduce((acc, line, i) => {
+    const sumOfline = line.reduce((sum, item, j) => {
+      if (i > 0 && matrix[i - 1][j] === 0) {
+        return sum;
+      }
+      return sum + item;
+    }, 0);
+    return acc + sumOfline;
+  }, 0);
+  return result;
 }
 
 module.exports = getMatrixElementsSum;
