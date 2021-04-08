a8e9cdd1f287c5281ef4e48b99286352e103433e
Vladimir Bessonov <bessmy@gmail.com>
2021-04-08 16:41:13 +0300

    feat: done 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let result = '';
+  let counter = 1;
+  let currentChar = '';
+  for (let i = 0; i <= arr.length; i += 1) {
+    if (currentChar === arr[i]) {
+      counter += 1;
+    } else {
+      const digit = counter === 1 ? '' : counter;
+      result = `${result}${digit}${currentChar}`;
+      currentChar = arr[i];
+      counter = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
